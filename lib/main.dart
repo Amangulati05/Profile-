29ef69f6ff45f7137665bb4cb788ea78d7580eb2
Amangulati05 <131166446+Amangulati05@users.noreply.github.com>
2024-06-08 11:03:05 +0530

    Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,138 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home:Scaffold(
-      backgroundColor: Colors.white10,
-      appBar: AppBar(
-        title: Text('MY Profile'),
-        backgroundColor: Colors.black12,
-
-      ),
-      body:  const Center(
-          child: Image(image: NetworkImage(
-            'https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg'
-          ),)
-
-      ),
-    )
-    ),
-  );
+  runApp(MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'WhatsApp UI',
+      theme: ThemeData(
+        primaryColor: Color(0xFF075E54),
 
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateMixin {
+ late  TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('WhatsApp'),
+        actions: <Widget>[
+          Icon(Icons.search),
+          SizedBox(width: 15),
+          Icon(Icons.more_vert),
+        ],
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: [
+            Tab(text: 'CHATS'),
+            Tab(text: 'STATUS'),
+            Tab(text: 'CALLS'),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          ChatsScreen(),
+          StatusScreen(),
+          CallsScreen(),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.message),
+        onPressed: () {},
+      ),
+    );
+  }
+}
+
+class ChatsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 20,
+      itemBuilder: (context, index) {
+        return ListTile(
+          leading: CircleAvatar(
+            backgroundColor: Colors.grey,
+          ),
+          title: Text('Contact $index'),
+          subtitle: Text('Last message preview...'),
+          trailing: Text('10:30 AM'),
+        );
+      },
+    );
+  }
+}
+
+class StatusScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: <Widget>[
+        ListTile(
+          leading: CircleAvatar(
+            backgroundColor: Colors.grey,
+          ),
+          title: Text('My Status'),
+          subtitle: Text('Tap to add status update'),
+        ),
+        Divider(),
+        ListTile(
+          leading: CircleAvatar(
+            backgroundColor: Colors.grey,
+          ),
+          title: Text('Recent updates'),
+          subtitle: Text('Contact status update...'),
+        ),
+      ],
+    );
+  }
+}
+
+class CallsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 10,
+      itemBuilder: (context, index) {
+        return ListTile(
+          leading: CircleAvatar(
+            backgroundColor: Colors.grey,
+          ),
+          title: Text('Contact $index'),
+          subtitle: Text('Yesterday, 10:30 AM'),
+          trailing: Icon(Icons.call),
+        );
+      },
+    );
+  }
+}
